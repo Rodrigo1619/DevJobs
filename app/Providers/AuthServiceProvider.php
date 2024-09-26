7e84fd5ca5c74d9202458b83f7917ecb336ea8c2
Rodrigo1619 <00067920@uca.edu.sv>
2024-09-25 20:38:37 -0600

    feat: changing email message

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -19,10 +22,18 @@ class AuthServiceProvider extends ServiceProvider
     /**
      * Register any authentication / authorization services.
      */
+    //este metodo se ejecuta cuando se inicia cuando el usuario le de click al boton de enviar correo de verificacion
     public function boot(): void
     {
         $this->registerPolicies();
 
-        //
+        //el callback recibe 2 parametros pero solo usaremos el segundo
+        VerifyEmail::toMailUsing(function($notifiable, $url){
+            return(new MailMessage)
+                ->subject('Verificar Cuenta')
+                ->line('Tu cuenta esta casi lista, presiona el boton para Verificar la cuenta')
+                ->action('Verificar Cuenta', $url)
+                ->line("Si no creaste la cuenta, ignora este mensaje");
+        });
     }
 }
